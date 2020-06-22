480f82968a7ff850dc545e64c6d40eba1dd363bc
gtulkn12 <45664554+gtulkn12@users.noreply.github.com>
2020-06-22 15:10:33 -0700

    implemented the UserSelectSquare()

--- src/com/tictactoe/tictactoe/UserSelectSquare.java
+++ src/com/tictactoe/tictactoe/UserSelectSquare.java
@@ -1,10 +1,17 @@
 package com.tictactoe.tictactoe;
 
 import java.util.Map;
+import java.util.Scanner;
 
 public class UserSelectSquare implements SelectSquareStrategy {
     @Override
     public int selectSquare(Map<Integer, String> squaresFilled) {
-        return 0;
+        Scanner scan = new Scanner(System.in);
+        int userPos = scan.nextInt();
+        while (!squaresFilled.get(userPos).equals("")) {
+            System.out.println("Position taken! Enter a correct Position");
+            userPos = scan.nextInt();
+        }
+        return userPos;
     }
 }
