1053bd88489222ff751d755e00a7e1ef99f1e2fd
Chase Tucker <tuck8106@gmail.com>
2020-06-22 13:11:20 -0700

    TicTacToeEndInTie implemented and tested

--- src/com/tictactoe/tictactoe/Grid.java
+++ src/com/tictactoe/tictactoe/Grid.java
@@ -1,10 +1,39 @@
 package com.tictactoe.tictactoe;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Grid {
-    private static String[] squaresFilled;
-    public EvaluateWinStrategy evaluateWinStrategy;
-    public EndInTieStrategy endInTieStrategy;
-    public Grid() {
+    private final Map<Integer, String> squaresFilled;
+    private final EvaluateWinStrategy evaluateWinStrategy;
+    private final EndInTieStrategy endInTieStrategy;
+
+    public Grid(EvaluateWinStrategy evaluateWinStrategy,
+                EndInTieStrategy endInTieStrategy) {
+        squaresFilled = new HashMap<>() {{
+            put(1, "");
+            put(2, "");
+            put(3, "");
+            put(4, "");
+            put(5, "");
+            put(6, "");
+            put(7, "");
+            put(8, "");
+            put(9, "");
+        }};
+        this.evaluateWinStrategy = evaluateWinStrategy;
+        this.endInTieStrategy = endInTieStrategy;
     }
 
+    public Map<Integer, String> getSquaresFilled() {
+        return squaresFilled;
+    }
+
+    public EvaluateWinStrategy getEvaluateWinStrategy() {
+        return evaluateWinStrategy;
+    }
+
+    public EndInTieStrategy getEndInTieStrategy() {
+        return endInTieStrategy;
+    }
 }
