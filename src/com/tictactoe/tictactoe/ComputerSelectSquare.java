9285c5e2ad744248341a1016075cad8f34cb6e66
Chase Tucker <tuck8106@gmail.com>
2020-06-22 17:23:33 -0700

    computerSelectSquare drafted. testing next

--- src/com/tictactoe/tictactoe/ComputerSelectSquare.java
+++ src/com/tictactoe/tictactoe/ComputerSelectSquare.java
@@ -1,45 +1,99 @@
 package com.tictactoe.tictactoe;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 public class ComputerSelectSquare implements SelectSquareStrategy {
-    private Set<Integer> userPossibleMoves;
-    private Set<Integer> computerPossibleMoves;
+    private Set<Integer> possibleMoves;
+    private Set<Integer> userMoves;
+    private Set<Integer> computerMoves;
 
     public ComputerSelectSquare() {
-        userPossibleMoves = new HashSet<>();
-        userPossibleMoves.add(1);
-        userPossibleMoves.add(2);
-        userPossibleMoves.add(3);
-        userPossibleMoves.add(4);
-        userPossibleMoves.add(5);
-        userPossibleMoves.add(6);
-        userPossibleMoves.add(7);
-        userPossibleMoves.add(8);
-        userPossibleMoves.add(9);
-        computerPossibleMoves = new HashSet<>();
-
+        possibleMoves = new HashSet<>();
+        possibleMoves.add(1);
+        possibleMoves.add(2);
+        possibleMoves.add(3);
+        possibleMoves.add(4);
+        possibleMoves.add(5);
+        possibleMoves.add(6);
+        possibleMoves.add(7);
+        possibleMoves.add(8);
+        possibleMoves.add(9);
+        userMoves = new HashSet<>();
+        computerMoves = new HashSet<>();
     }
 
     @Override
     public int selectSquare(Map<Integer, String> squaresFilled) {
-        int selection = 0;
         // identify set of possible moves
         for(Map.Entry<Integer, String> squaresFilledRow : squaresFilled.entrySet()) {
-
+            int square = squaresFilledRow.getKey();
+            String token = squaresFilledRow.getValue();
+            if(token != "") {
+                possibleMoves.remove(square);
+                if(token.equals("X")) {
+                    userMoves.add(square);
+                } else {
+                    computerMoves.add(square);
+                }
+            }
         }
 
-        // if move to win possible, make that move
+        // if move to win or block opponent win possible, move there
+        int winMove = winOrBlockMove();
+        if(winMove != 0) {
+            return winMove;
+        }
+        // otherwise, return best possible move
+        else return bestPossibleMove();
+    }
 
-        // identify opponent possible moves
+    private int winOrBlockMove() {
+        for(Map.Entry<Integer, Map<Integer, Integer>> possibilityKey :
+                Grid.winPossibilities.entrySet()) {
+            if(computerMoves.contains(possibilityKey.getKey())) {
+                for(Map.Entry<Integer, Integer> possibilityVal :
+                        possibilityKey.getValue().entrySet()) {
+                    if(computerMoves.contains(possibilityVal.getKey())) {
+                        return possibilityVal.getValue();
+                    } else if(computerMoves.contains(possibilityVal.getValue())) {
+                        return possibilityVal.getKey();
+                    }
+                }
+            }
+        }
+        for(Map.Entry<Integer, Map<Integer, Integer>> possibilityKey :
+                Grid.winPossibilities.entrySet()) {
+            if(userMoves.contains(possibilityKey.getKey())) {
+                for(Map.Entry<Integer, Integer> possibilityVal :
+                        possibilityKey.getValue().entrySet()) {
+                    if(userMoves.contains(possibilityVal.getKey())) {
+                        return possibilityVal.getValue();
+                    } else if(userMoves.contains(possibilityVal.getValue())) {
+                        return possibilityVal.getKey();
+                    }
+                }
+            }
+        }
+        return 0;
+    }
 
-        // block if opponent win eminent
-
-        // select move with best possibility of win
-
-
-        return selection;
+    private int bestPossibleMove() {
+        // if two squares are open in line with currently selected square, select the first
+        // one found
+        for(int possibleMove : computerMoves) {
+            Map<Integer, Integer> possibilities = Grid.winPossibilities.get(possibleMove);
+            for(Map.Entry<Integer, Integer> possibility : possibilities.entrySet()) {
+                if(possibleMoves.contains(possibility.getKey()) &&
+                    possibleMoves.contains(possibility.getValue())) {
+                    return possibility.getKey();
+                }
+            }
+        }
+        // if no two squares in line available, select next possible move
+        Iterator<Integer> nextPossibility = possibleMoves.iterator();
+        return nextPossibility.next();
     }
 }
